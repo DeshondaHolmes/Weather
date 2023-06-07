76cb02c6a1b3d556afc57f68506392875e8279d8
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-07 10:17:01 -0400

    remove old table code and add table body

--- scripts/cities.js
+++ scripts/cities.js
@@ -1,24 +1,34 @@
 "use strict";
 
+
+//define cityDropDown as html id for dropdown list of cities
 const cityDropDown = document.getElementById("cityDropDown");
 
+const tableBody = document.getElementById("tableBody");
 
+
+//arrow function to execute function when browser finish loading 
 window.onload = () => {
 
+    //on dropdown change a function will execute 
     cityDropDown.onchange = onCityDropDownChange;
 
+
+    //loop through cities array creating a new option for each city in dropdown 
     for (let city of cities) {
 
+        //newOption defined as city.name 
         let newOption = new Option(city.name);
+        //create city name options in dropdown 
         cityDropDown.appendChild(newOption);
 
-        console.log(cityDropDown);
     }
 
 };
 
-
+//onCityDropDownChange function
 function onCityDropDownChange (){
+
 
     let selectedCity = cityDropDown.value;
 
@@ -33,5 +43,43 @@ function onCityDropDownChange (){
 
 
 function createCityTable(){
+   let stationLookupUrl = `https://api.weather.gov/points/${selectedCity.latitude},${selectedCity.longitude}`;
+
+   
+       fetch(stationLookupUrl)
+           .then(response => response.json())
+           .then(data => {
+        });
+
 
 }
+
+//onCityDropDownChange function
+// function onCityDropDownChange() {
+
+
+//     let selectedCity = cityDropDown.value;
+
+//     const cityFilter = cities.filter(city => city.name === selectedCity);
+
+
+// }
+
+
+// function createCityTable(city) {
+
+   
+//         let row = table.insertRow();
+//         let cell1 = row.insertCell();
+//         let cell2 = row.insertCell();
+//         let cell3 = row.insertCell();
+
+
+//         cell1.innerHTML = data[i].city;
+//         cell2.innerHTML = data[i].city;
+//         cell3.innerHTML = data[i].city;
+//     }
+
+
+// }
+
