4e0daaec023ddab65e736374a6c3875714cd7063
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-07 13:02:49 -0400

    remove old code add comments

--- scripts/cities.js
+++ scripts/cities.js
@@ -6,7 +6,8 @@ const cityDropDown = document.getElementById("cityDropDown");
 
 const tableBody = document.getElementById("tableBody");
 
-let selectedCity = ""; // Add a variable to store the selected city
+aCitySelected = cityDropDown.value;
+ // Add a variable to store the selected city
 
 //arrow function to execute function when browser finish loading 
 window.onload = () => {
@@ -16,10 +17,10 @@ window.onload = () => {
 
 
     //loop through cities array creating a new option for each city in dropdown 
-    for (let aCity of cities) {
+    for (let City of cities) {
 
         //newOption defined as city.name 
-        let newOption = new Option(aCity.name);
+        let newOption = new Option(City.name);
         //create city name options in dropdown 
         cityDropDown.appendChild(newOption);
 
@@ -29,13 +30,13 @@ window.onload = () => {
 
 function showGeographicLocation() {
 
-    selectedCity = cityDropDown.value; // Update the selected city
+    aCitySelected = cityDropDown.value; // Update the selected city
 
-    if (selectedCity !== "") {
-        const city = cities.find(city => city.name === selectedCity);
+    if (aCitySelected !== "") {
+        const aCitySeletected = cities.find(city => city.name === aCitySeletected);
 
-        const latitude = city.latitude;
-        const longitude = city.longitude;
+        const latitude = aCitySeletected.latitude;
+        const longitude = aCitySeletected.longitude;
 
         // console.log(latitude);  // Outputs: Latitude
         // console.log(longitude); // Outputs: Longitude
@@ -43,7 +44,7 @@ function showGeographicLocation() {
         // latitudeOutput.innerHTML = latitude;
         // longitudeOutput.innerHTML = longitude;
 
-        
+        showGeographicLocation();
         createWeatherTable();
         getWeatherApiForCity(latitude, longitude);
     }
@@ -51,11 +52,11 @@ function showGeographicLocation() {
 
 //onCityDropDownChange function
 
-function getWeatherApiForCity(latitude,longitude) {
+function getWeatherApiForCity() {
 
    
-    let stationLookupUrl = `https://api.weather.gov/points/${latitude},${longitude}`;
-    let selectedCity = cities.find(city => city.name === selectedCity);
+    let stationLookupUrl = `https://api.weather.gov/points/${aCitySelected.latitude},${aCitySelected.longitude}`;
+    let aCitySelected = cities.find(city => city.name === aCitySelected);
     fetch(stationLookupUrl)
     .then(response => response.json())
     .then(data => {
@@ -66,31 +67,31 @@ function getWeatherApiForCity(latitude,longitude) {
 
 function getWeather(weatherApiUrl) {
     fetch(weatherApiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const forecastArray = data.properties.periods;
-            createWeatherTable(forecastArray);
-        });
+    .then(response => response.json())
+    .then(data => {
+        const forecastArray = data.properties.periods;
+        createWeatherTable(forecastArray);
+    });
 
 }
 
 
 
 function createWeatherTable(forecastArray) {
-    tableBody.innerHTML = "";
+    // tableBody.innerHTML = "";
 
 
     for (let cityWeather of forecastArray){
 
-        let row = table.insertRow(-1);
+        let row = tableBody.insertRow(-1);
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell();
         let cell3 = row.insertCell();
 
     
     
-        cell1.innerHTML = cityWeather.name;
-        cell2.innerHTML = cityWeather.temperature;
+        cell1.innerHTML = "Hello" + cityWeather.name;
+        cell2.innerHTML = cityWeather.temperature + cityWeather.temperatureUnit;
         cell3.innerHTML = cityWeather.temperature + cityWeather.temperatureUnit;
 
 
